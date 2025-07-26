7b9166b64d9b114efce100b55440c091aa3670f4
Nouh1408 <ahmedinouh@gmail.com>
2025-07-27 00:58:06 +0300

    setup

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,2 +1,12 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb:127.0.0.1:27017/football') 
+export default function connectDB(){
+    mongoose.connect('mongodb://127.0.0.1:27017/football').then(()=>{
+    console.log('====================================');
+    console.log("connecteed to DB");
+    console.log('====================================');
+}).catch((err)=>{
+    console.log('====================================');
+    console.log("fail to connect to Db",err.message);
+    console.log('====================================');
+})
+}
--- src/app.controller.js
+++ src/app.controller.js
@@ -1,5 +1,7 @@
 //ctrl requests
-
+import connectDB from "./DB/connection.js"
 export default function bootstrap(app,express){
+    connectDB()
     app.use(express.json())
+
 }
