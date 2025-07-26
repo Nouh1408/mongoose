75cd135817b8723008982252dfdfa278d8803890
Nouh1408 <ahmedinouh@gmail.com>
2025-07-27 00:51:14 +0300

    mongoose baby steps

---
+++ src/app.controller.js
@@ -0,0 +1,5 @@
+//ctrl requests
+
+export default function bootstrap(app,express){
+    app.use(express.json())
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import bootstrap from './app.controller.js'
+const app =express()
+const port =3000
+bootstrap(app,express)
+
+app.listen(port, ()=>{
+    console.log("server running on port ",port);
+    
+})
