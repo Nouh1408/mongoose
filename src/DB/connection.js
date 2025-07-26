5a4da401f8912c166992f230c92f2f205f94d120
Nouh1408 <ahmedinouh@gmail.com>
2025-07-27 01:24:30 +0300

    connection mongoosde

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 export default function connectDB(){
-    mongoose.connect('mongodb://127.0.0.1:27017/football').then(()=>{
+    mongoose.connect('mongodb://127.0.0.1:27017/football',{serverSelectionTimeoutMS:3000}).then(()=>{
     console.log('====================================');
     console.log("connecteed to DB");
     console.log('====================================');
@@ -10,3 +10,5 @@ export default function connectDB(){
     console.log('====================================');
 })
 }
+const User = model("User", new Schema({name:String}))
+User.create({name:"Ahmed"})
