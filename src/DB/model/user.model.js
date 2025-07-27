df6ecbda1dffedc9382bc0cd705bfd94b35a57e8
Nouh1408 <ahmedinouh@gmail.com>
2025-07-27 14:48:41 +0300

    finished schema and model for USer

---
+++ src/DB/model/user.model.js
@@ -0,0 +1,9 @@
+import { model, Schema } from "mongoose";
+//schema
+const schema = new Schema({
+    username: String,
+    email:String,
+    password:String
+},{})
+//model
+export const User = model("User", schema)
