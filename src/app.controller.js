9b97b2396937f30738333dd211d05c8b57be3397
Nouh1408 <ahmedinouh@gmail.com>
2025-07-31 16:52:58 +0300

    the middleware must be before routing

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,7 +3,8 @@ import {connectDB} from "./DB/connection.js"
 import authRouter from "./module/auth/auth.controller.js"
 export default function bootstrap(app,express){
     connectDB()
+    app.use(express.json())
     app.use('/auth', authRouter)
-    app.use(express.json()) //
+     //
 
 }
