9f5d1d0dbbbb7e0864804e358e28ca8c934f756a
Nouh1408 <ahmedinouh@gmail.com>
2025-08-02 21:16:40 +0300

    validate user model schema

--- src/DB/model/user.model.js
+++ src/DB/model/user.model.js
@@ -2,15 +2,27 @@ import { model, Schema } from "mongoose";
 import mongoose from "mongoose";
 
 //schema
-const schema = new Schema({
+const schema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        minlength: 3
+      type: String,
+      required: true,
+      minlength: [3, "must be at least 3 characters long"],
+      trim: true,
     },
-    email:String,
-    password:String,
-    phonenumber:String
-},{})
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    // password: { type: String, required: true }, // Uncomment this line if you want to enforce password requirement
+    password: { type: String, required: true },
+    phonenumber: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 //model
-export const User = mongoose.model("User", schema)
+export const User = mongoose.model("User", schema);
